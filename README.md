f3d2d005a4fae5e56e9cf68ca95d89ea8fbccd3f
Paul Cowie <paul.cowie@glasgow.ac.uk>
2018-07-07 18:31:33 +0100

    Working, add README

---
+++ README.md
@@ -0,0 +1,50 @@
+# Ghimlink
+
+A simple command-line application for generating a Github-Flavoured-Markdown image link for a file in your repository.
+
+## Simple Usage
+
+For example, if we want to link to `gimli.png` and `link.jpg` in this repository's `res/` directory, we could do so as follows:
+
+```bash
+ghimlink ./res/gimli.png
+Enter alt text: Gimli
+![Gimli](https://github.com/SCOTPAUL/ghimlink/raw/master/res/gimli.png)
+
+ghimlink ./res/link.jpg 
+Enter alt text: Link
+![Link](https://github.com/SCOTPAUL/ghimlink/raw/master/res/link.jpg)
+```
+
+Giving us:
+
+![Gimli](https://github.com/SCOTPAUL/ghimlink/raw/master/res/gimli.png)
+![Link](https://github.com/SCOTPAUL/ghimlink/raw/master/res/link.jpg)
+
+Ghimlink will handle retreiving your Github remote data automatically (as long as you have a valid `origin` remote on Github).
+
+## Advanced Usage
+
+```
+ghimlink 0.1.0
+Paul Cowie <paul.cowie@glasgow.ac.uk>
+Generates a Github-Flavoured-Markdown formatted image link to an image in the same repository.
+
+USAGE:
+    ghimlink [OPTIONS] <IMAGE_PATH>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -a, --alt_text <ALT_TEXT>     Sets the alt-text for the image
+    -b, --branch <BRANCH_NAME>    Sets the file's branch name (defaults to master)
+
+ARGS:
+    <IMAGE_PATH>    Path to the image file
+```
+
+## TODO
+
+- Add support for remotes other than `origin`.
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,99 @@
 extern crate git2;
+extern crate regex;
+#[macro_use] extern crate text_io;
+#[macro_use] extern crate clap;
+
 use git2::{Repository};
+use regex::Regex;
+use std::path::{Path, PathBuf};
+use std::io;
+use std::io::Write;
 
-
-
-fn main() {
-    println!("Hello, world!");
-
-    match get_remote_name() {
-        Ok(name) => println!("{}", name),
-        Err(e) => println!("{:?}", e)
-    }
+struct ImageDetails {
+    alt_text: String,
+    image_path: PathBuf
 }
 
-fn get_remote_name() -> Result<String, git2::Error> {
+fn main() {
+    let matches = app_from_crate!()
+                    .args_from_usage("-b --branch=[BRANCH_NAME] 'Sets the file's branch name (defaults to master)'
+                                      -a --alt_text=[ALT_TEXT] 'Sets the alt-text for the image'
+                                     <IMAGE_PATH> 'Path to the image file'")
+                    .get_matches();
+
+    let image_path = matches.value_of("IMAGE_PATH").unwrap();
+    let branch_name = matches.value_of("branch").unwrap_or("master");
+    let alt_text = matches.value_of("alt_text");
+
+    let details = get_image_link_details(&image_path, alt_text);
+
+    match details {
+        Some(details) => {
+            match get_remote_url() {
+                Ok(remote_name) => println!("![{}]({}{}/{}/{})",
+                    details.alt_text,
+                    remote_name,
+                    "raw",
+                    branch_name,
+                    details.image_path.to_str().unwrap()),
+                Err(e) => println!("{:?}", e)
+            };
+        },
+        None => eprintln!("Error: Invalid image details")
+    };
+
+}
+
+fn get_image_link_details(image_path: &str, alt_text_option: Option<&str>) -> Option<ImageDetails> {
+    let path = get_relative_image_path(image_path)?;
+
+    let alt_text = match alt_text_option {
+        Some(alt_text) => alt_text.to_string(),
+        None => {
+            print!("Enter alt text: ");
+            io::stdout().flush().unwrap();
+            let alt_text: String = read!();
+            alt_text
+        }
+    };
+
+    Some(ImageDetails { alt_text: alt_text, image_path: path })
+}
+
+fn get_relative_image_path(image_path: &str) -> Option<PathBuf> {
+    let full_path = match Path::new(image_path).canonicalize() {
+        Ok(path) => Some(path),
+        Err(_e) => None
+    }?;
+
+    let repo = match Repository::open_from_env() {
+        Ok(repo) => Some(repo),
+        Err(_e) => None
+    }?;
+
+    let repo_path = repo.path().parent()?;
+    let relative_path = full_path.strip_prefix(repo_path).unwrap().to_path_buf();
+
+    Some(relative_path)
+
+}
+
+fn get_remote_url() -> Result<String, git2::Error> {
     let repo = Repository::open_from_env()?;
-
     let remote = repo.find_remote("origin")?;
+    let name = remote.url().unwrap().to_string();
+    
+    let https_name = if name.starts_with("git@") {
+        let re = Regex::new(r"git@github\.com:(?P<username>.+)/(?P<repo_name>[^\.]+)(?:\.git)?").unwrap();
 
-    let name = remote.url().unwrap();
+        let matches = re.captures(&name).unwrap();
 
-    Ok(name.to_string())
+        format!("https://github.com/{}/{}/", &matches["username"], &matches["repo_name"])
 
+    }
+    else {
+        name
+    };
+
+    Ok(https_name)
 }
